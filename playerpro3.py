cc5f84300788c5abdcdd7131f669b045874d3677
ansaari <47440555+ansaari@users.noreply.github.com>
2019-02-19 14:01:35 +0530

    Update playerpro3.py

--- playerpro3.py
+++ playerpro3.py
@@ -1,8 +1,8 @@
-Number = int(input("Please Enter any Number: "))    
+Number = int(input(" "))    
 Reverse = 0    
 while(Number > 0):    
     Reminder = Number %10    
     Reverse = (Reverse *10) + Reminder    
     Number = Number //10    
      
-print("\n Reverse of entered number is = %d" %Reverse) 
+print("%d" %Reverse) 
